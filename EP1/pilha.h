b86cbcaeaf727d81ae6f6f921d642074987be1bd
Henrique Cerquinho <henriquecerquinho@gmail.com>
2017-09-07 15:11:36 -0300

    Fiz boa parte do round robin Mudei a funçao de threads pra funcionar de um jeito dependendo do tipo de escalonador adicionei uns frees ai

--- EP1/pilha.h
+++ EP1/pilha.h
@@ -7,6 +7,7 @@
 typedef struct{
 	float dt; /* Tempo real necessário */
 	float et; /* Tempo restante de execução necessário */
+	float quantum;
 	char *name;
 	int i;
 } process;
@@ -25,6 +26,12 @@ pilha *criaPilha(int MAX);
 /* Insere uma line x na pilha de forma a ficar ordenada pelo
 // tempo restante de execução (dt - et)  */
 void insereOrdenado(pilha *p, process *x);
+
+/* insere x no topo da pilha */
+void empilha(pilha *p, process *x);
+
+/* remove o elemento no indice i da pilha e reajusta os elementos seguintes */
+process *removePros(pilha *p, int i);
 
 process *desempilha(pilha *p);
 
