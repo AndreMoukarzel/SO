7cc645309ee6020d9468513e84e277f3274dbbab
Henrique Cerquinho <henriquecerquinho@gmail.com>
2017-09-22 17:16:51 -0300

    Fiz mais ou menos o esqueletos das funçoes que vamos usar Será que não é melhor deixar a função da thread no mesmo arquivo da main? :thinking:

--- EP2/ciclista.c
+++ EP2/ciclista.c
@@ -1,5 +1,16 @@
 #include "ciclista.h"
 
-void *threadCiclista(void* arg) {
+/* nao seria melhor deixar essa funçao na corrida.c? */
+void *threadCiclista(void * arg) {
+	int *id;
+	ciclista c;
+	id = (int *) arg;
+
+	c.n = 1;
+	c.v = 30; /* todos começam a 30km/h */
+	c.id = id;
+	pista[0][id] = id; /* ciclista esta em 0 metros e na pista de numero id */
+
+	printf("dentro da thread: %d\n", *id);
 	return NULL;
 }
--- EP2/corrida.c
+++ EP2/corrida.c
@@ -6,13 +6,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <pthread.h>
 #include <sys/wait.h>
 #include <sys/types.h>
 #include <time.h>
+#include "ciclista.h"
 
 #define MAX_LENGTH 1024
 
+/*pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;*/
+/* matriz da pista, de largura d e altura n */
+int **pista;
+
+
+void simulador(int d, int n, int v){
+	int th, i, *id;
+	pthread_t *ciclistas = malloc(n * sizeof(pthread_t));
+
+	id = &i;
+
+	for (i = 0; i < n; i++){
+		printf("%d\n", *id);
+		if ((th = pthread_create(&ciclistas[i], NULL, threadCiclista, (void *) id)))
+			printf("Failed to create thread %d\n", th);
+	}
+	for (i = 0; i < n; i++)
+		pthread_join(ciclistas[i], NULL);
+
+	free(ciclistas);
+}
+
 
 int main(int argc, char **argv) {
+	int d, n, v, i;
+	d = atoi(argv[1]);
+	n = atoi(argv[2]);
+	v = atoi(argv[3]);
+
+	/* essa alocagem ta certa?>?? */
+	pista = malloc(d*n*sizeof(int));
+	for (i = 0; i < d; i++)
+		pista[i] = malloc(n*sizeof(int));
+
+	simulador(d, n, v);
+
+	for (i = 0; i < d; i++)
+		free(pista[i]);
+	free(pista);
 	return 0;
 }
