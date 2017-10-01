4acb597496212dccbad18f801ace71663b2c1b48
Henrique Cerquinho <henriquecerquinho@gmail.com>
2017-10-01 13:25:26 -0300

    Coloquei a funçao do ciclista junto com a main e botei as barreiras Ciclistas agora inicializam corretamente

--- EP2/corrida.c
+++ EP2/corrida.c
@@ -3,6 +3,7 @@
 // Nome: Henrique Cerquinho								NUSP: 9793700
 ////////////////////////// COMO RODAR /////////////////////////////*/
 
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -13,17 +14,37 @@
 
 #define MAX_LENGTH 1024
 
-/*pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;*/
+/* Mutex que sera usado na inicializaçao dos ciclistas */
+pthread_mutex_t init_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_barrier_t barreira;
 metro* pista;
+
+
+void *threadCiclista(void * arg) {
+	/* Atribui o argumento ao id do ciclista */
+	int *temp, id;
+	temp = (int *) arg;
+	id = *temp;
+
+	pthread_mutex_unlock(&init_mutex); /* Pode iniciar outro ciclista */
+
+	pthread_barrier_wait(&barreira); /* Barreira para inicializaçao */
+	printf("dentro da thread: %d\n", id);
+	return NULL;
+}
 
 
 void simulador(int d, int n, int v){
 	int th, i, *id;
 	pthread_t *ciclistas = malloc(n * sizeof(pthread_t));
+	pthread_barrier_init(&barreira, NULL, (unsigned) d);
 
 	id = &i;
 
 	for (i = 0; i < n; i++){
+		/* Fecha o mutex para passar o argumento e ele nao ser mudado
+		// enquanto o ciclista o grava no id */
+		pthread_mutex_lock(&init_mutex);
 		printf("%d\n", *id);
 		if ((th = pthread_create(&ciclistas[i], NULL, threadCiclista, (void *) id)))
 			printf("Failed to create thread %d\n", th);
@@ -41,9 +62,9 @@ int main(int argc, char **argv) {
 	n = atoi(argv[2]);
 	v = atoi(argv[3]);
 
+	simulador(d, n, v);
 	pista = criaPista(d);
 
-	simulador(d, n, v);
 
 	destroiPista(pista, d);
 
