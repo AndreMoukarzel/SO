39d1920bb955da31b3731b9409802d7b8b729b34
Detril <andremoukarzel@hotmail.com>
2017-10-01 16:01:50 -0300

    Ciclistas quebrando não mais travam a barreira

--- EP2/corrida.c
+++ EP2/corrida.c
@@ -20,23 +20,36 @@ pthread_mutex_t volta_mutex, init_mutex = PTHREAD_MUTEX_INITIALIZER;
 ciclista* ciclistas;
 metro* pista;
 int num_voltas;
+int cic_ativos; /* Número de ciclistas que não quebraram */
+int fim = 0;
 /*********************************************************************/
+
+
+void *threadDummy() {
+	while (!fim)
+		pthread_barrier_wait(&barreira);
+
+	return NULL;
+}
 
 
 void *threadCiclista(void * arg) {
 	/* Atribui o argumento ao id do ciclista */
 	ciclista *temp, c;
+	pthread_t dummy;
+
 	temp = (ciclista *) arg;
 	c = *temp;
 	pthread_mutex_unlock(&init_mutex);
 
-	printf("dentro da thread: %d\n", c.id);
 	while (c.volta < num_voltas) {
 		if ((c.volta % 15) == 0)
-			if (quebraCiclista(c))
+			if (quebraCiclista(c)) {
+				pthread_create(&dummy, NULL, &threadDummy, NULL);
 				return NULL;
+			}
 
-		c.volta =+ 1;
+		c.volta += 1;
 		pthread_barrier_wait(&barreira); /* Barreira para inicializaçao */
 	}
 	return NULL;
@@ -56,18 +69,19 @@ void corrida(int d, int n){
 
 	preparaLargada(d, n);
 	pthread_barrier_init(&barreira, NULL, n);
+	cic_ativos = n;
 
 	/* Dispara as threads */
 	for (i = 0; i < n; i++) {
-		printf("%d\n", ciclistas[i].id);
 		pthread_mutex_lock(&init_mutex);
 		if ((th = pthread_create(&thread[i], NULL, threadCiclista, (void *) &ciclistas[i])))
 			printf("Failed to create thread %d\n", th);
 	}
 
 	/* Espera a corrida acabar */
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n; i++) {
 		pthread_join(thread[i], NULL);
+	}
 
 
 	/* Imprimir resultados aqui */
