e2e56fb5e136e04ccd0215f85de097d4ac699a1f
Henrique Cerquinho <henriquecerquinho@gmail.com>
2017-10-28 15:46:50 -0200

    Fiz uma função para ler o arquivo e guardar os dados

--- EP3/ep3.py
+++ EP3/ep3.py
@@ -1,37 +1,58 @@
 #####################################################################
-# Nome: Andre Ferrari Moukarzel						NUSP: 9298166	#
-# Nome: Henrique Cerquinho							NUSP: 9793700	#
+# Nome: Andre Ferrari Moukarzel                     NUSP: 9298166	#
+# Nome: Henrique Cerquinho                          NUSP: 9793700	#
 #####################################################################
 
 import readline
+import time
 
 ger = {1: "Best Fit", 2: "Worst Fit", 3: "Quick Fit"}
 sub = {1: "Optimal", 2: "FIFO", 3: "LRU.2", 4: "LRU.4"}
 
-while True:
-	linha = raw_input("(ep3): ")
-	linha = linha.split()
 
-	if linha[0] == "carrega":
-		print "Carregando arquivo", linha[1]
-		try:
-			open(linha[1])
-		except Exception:
-			print "Arquivo nao pode ser carregado"
+def readFile(nome):
+	dados = []
+	arquivo = open(nome, "r")
 
-	elif linha[0] == "espaco":
-		print "Gerenciamento de espaco definido para", ger[int(linha[1])]
-		# Settar alguma variavel como linha[1] aqui
+	for linha in arquivo:
+		dados.append(linha.split())
 
-	elif linha[0] == "substitui":
-		print "Algoritmo de substituicao definido para", sub[int(linha[1])]
-		# Settar alguma variavel como linha[1] aqui
+	# Converte todas as string de numero para ints
+	for i in range(len(dados)):
+		for j in range(len(dados[i])):
+			try:
+				dados[i][j] = int(dados[i][j])
+			except:
+				continue
+	return dados
 
-	elif linha[0] == "executa":
-		print "BLIP BLOP BLUP"
+
+def main():
 	
-	elif linha[0] == "sai":
-		break
+	while True:
+		linha = raw_input("[ep3]: ")
+		linha = linha.split()
 
-	else:
-		print "Comando invalido"
+		if linha[0] == "carrega":
+			print ("Carregando arquivo " + linha[1])
+			dados = readFile(linha[1])
+			print(dados)
+
+		elif linha[0] == "espaco":
+			print ("Gerenciamento de espaco definido para " + ger[int(linha[1])])
+			# Settar alguma variavel como linha[1] aqui
+
+		elif linha[0] == "substitui":
+			print ("Algoritmo de substituicao definido para " + sub[int(linha[1])])
+			# Settar alguma variavel como linha[1] aqui
+
+		elif linha[0] == "executa":
+			print ("BLIP BLOP BLUP")
+
+		elif linha[0] == "sai":
+			break
+
+		else:
+			print ("Comando invalido")
+	return
+main()
