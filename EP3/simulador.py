24fcdc91e87445fc33856d8559a1667944e08076
Detril <andremoukarzel@hotmail.com>
2017-11-11 10:55:04 -0200

    Prototipo de bestFit

--- EP3/simulador.py
+++ EP3/simulador.py
@@ -2,7 +2,7 @@
 # Nome: Andre Ferrari Moukarzel                     NUSP: 9298166	#
 # Nome: Henrique Cerquinho                          NUSP: 9793700	#
 #####################################################################
-import struct
+import math
 
 class Memoria:
 	tam = 0
@@ -44,6 +44,10 @@ class Memoria:
 		f.close()
 
 
+	def compactar(self):
+		print ("Haha eu n fiz o compactar ainda lololkillmelololol")
+
+
 	def bytes_to_int(self, bytes):
 		return int(bytes.encode('hex'), 16)
 
@@ -59,5 +63,29 @@ def simula(arquivo, espaco, subst, intervalo):
 	s = int(linha[2]) # Tamanho da pagina
 	p = int(linha[3]) # Tamanho da unidade de alocacao
 
-	Memoria('/tmp/ep3.mem', fis_total)
-	Memoria('/tmp/ep3.vir', vir_total)
+	fis = Memoria('/tmp/ep3.mem', fis_total)
+	vir = Memoria('/tmp/ep3.vir', vir_total)
+
+	bestFit(vir, "ronaldo", int(math.ceil(8/s)))
+	print (vir.read())
+
+
+# Insere p com p_paginas na memoria especificada
+def bestFit(memoria, p_nome, p_paginas):
+	mem = memoria.read()
+	best_index = 0
+	best_tam = memoria.tam
+	ini = 0
+
+	for i in range(memoria.tam):
+		# Deve considerar o fim da memoria para n perder intervalos ao final
+		if mem[i] != 128 or i == memoria.tam - 1: # 128 = -1 em binario
+			if i - ini < best_tam and i - ini > p_paginas:
+				best_tam = i - ini
+				best_index = ini
+			ini = i + 1
+
+	for i in range(p_paginas):
+		mem[best_index + i] = 3 # PID do processo no lugar de 3 aqui
+
+	memoria.write(mem)
