6588b60e6f5e896f8ec1c64cd83880593c76ac49
Detril <andremoukarzel@hotmail.com>
2017-11-16 17:15:18 -0200

    Reestruturação MUITO BOLADA  da memória

---
+++ EP3/memoria.py
@@ -0,0 +1,390 @@
+#####################################################################
+# Nome: Andre Ferrari Moukarzel                     NUSP: 9298166	#
+# Nome: Henrique Cerquinho                          NUSP: 9793700	#
+#####################################################################
+import math
+import estruturas as es
+from bitarray import bitarray
+
+class Memoria:
+	tam = 0
+	arquivo = 0
+	bitmap = 0
+	ua = 0 # unidade de alocacao
+	pag = 0 # tamanho da pagina
+	ll = None # Lista ligada dos espacos livres
+
+
+	def __init__(self, nome, tamanho, ua, pag):
+		self.tam = tamanho
+		self.arquivo = nome
+		self.ua = ua
+		self.pag = pag
+
+		self.criaArquivo()
+		self.bitmap = ((tamanho/ua) + 1) * bitarray('0')
+		self.atualizaLL()
+
+
+	def criaArquivo(self):
+		f = open(self.arquivo, 'w+b') # Constroi o arquivo
+		f.close()
+		l = []
+		for i in range (self.tam):
+			l.append(128) # Preenche com -1
+		self.write(l)
+
+
+	def imprime(self):
+		l = self.read()
+		print ('Indice\t|\tPID\t|\tBitmap')
+		for i in range(self.tam):
+			bit = str(int(self.bitmap[int(i/self.ua)]))
+			if l[i] == 128:
+				if not i%self.ua:
+					print (str(i) + '\t|\t' + str(-1) + '\t|\t' + bit)
+				else:
+					print (str(i) + '\t|\t' + str(-1) + '\t|')
+			else:
+				if not i%self.ua:
+					print (str(i) + '\t|\t' + str(l[i]) + '\t|\t' + bit)
+				else:
+					print (str(i) + '\t|\t' + str(l[i]) + '\t|')
+
+
+	# Insere processo de PID que ocupa by bytes, comecando na posicao pos
+	def insere(self, pid, pos, by):
+		l = self.read()
+		b = int(math.ceil(by/float(self.ua)) * self.ua) # Unidades de alocacao sao preenchidas
+
+		for i in range(pos, pos + b): # Atualiza bitmap
+			l[i] = pid
+			self.bitmap[int(i/self.ua)] = True
+
+		self.atualizaLL()
+		self.write(l)
+
+
+	# Remove processo com o PID dado
+	def remove(self, pid):
+		l = self.read()
+
+		# Encontra a posicao em que o processo comeca
+		ret = -1
+		i = 0
+		for i in range(0, self.tam, self.pag):
+			if l[i] == pid:
+				ret = i
+				break
+		while l[i] == pid and i < self.tam:
+			l[i] = 128
+			self.bitmap[int(i/self.ua)] = False
+			i += 1
+
+		self.atualizaLL()
+		self.write(l)
+		return ret # -1 eh erro
+
+
+	# Devolve a memoria como uma lista de PIDs (int)
+	def read(self):
+		lista = []
+
+		with open(self.arquivo, "rb") as f:
+			byte = f.read(1)
+			while byte != "":
+				lista.append(self.bytes_to_int(byte))
+				byte = f.read(1)
+		f.close()
+
+		return lista
+
+
+	# Define a memoria como a lista de PIDs passada como argumento
+	def write(self, lista):
+		binarios = bytearray(lista)
+		f = open(self.arquivo, 'r+')
+		f.write(binarios)
+		f.close()
+
+
+	# Compacta memoria
+	def compactar(self):
+		mem = self.read()
+		compactado = False
+		ocupado = self.tam
+
+		while not compactado:
+			# Procura a primeiro pagina livre
+			cheio = True
+			for i in range(0, self.tam, self.pag):
+				if mem[i] == 128:
+					cheio = False
+					livre = i
+					break
+			# Ja esta compactado se esta cheio
+			if cheio:
+				break
+
+			# Procura a proxima pagina ocupada
+			for i in range(livre, self.tam, self.pag):
+				if mem[i] != 128:
+					ocupado = i
+					break
+
+
+			# Move os blocos ocupados para a esquerda
+			while ocupado < self.tam:
+				# Move o bloco inteiro
+				while mem[ocupado] != 128:
+					mem[livre] = mem[ocupado]
+					mem[ocupado] = 128
+
+				livre += 1
+				ocupado += 1
+
+			# Checa se compactou
+			fim = 0
+			for i in range(0, self.tam, self.pag):
+				compactado = True
+				if mem[i] == 128:
+					fim = 1
+				if fim == 1 and mem[i] != 128:
+					compactado = False
+					break
+
+		for i in range(0, self.tam, self.ua): # Atualiza o bitmap
+			if mem[i] == 128:
+				self.bitmap[int(i/self.ua)] = False
+			else:
+				self.bitmap[int(i/self.ua)] = True
+
+		self.write(mem)
+
+
+	def bytes_to_int(self, bytes):
+		return int(bytes.encode('hex'), 16)
+
+
+	# "Atualiza" a lista ligada que representa o bitmap da memoria criando uma
+	# nova lista do zero e retorna a sua raiz
+	def atualizaLL(self):
+		raiz = es.listaLigada()
+		last = es.listaLigada() # Ultimo elemento da lista
+		ini, fim = 0, 0
+
+		while fim < len(self.bitmap) and ini < len(self.bitmap):
+			# Procura um bloco livre
+			while ini < len(self.bitmap) and self.bitmap[ini]: # bitmap[ini] = 1
+				ini += 1
+			fim = ini + 1
+			# E define seu tamanho
+			while fim < len(self.bitmap) and not self.bitmap[fim]: # bitmap[fim] = 0
+				fim += 1
+
+			if ini < len(self.bitmap) and fim <= len(self.bitmap):
+				if raiz.pos == -1:
+					raiz.iniRaiz(ini * self.ua, (fim - ini) * self.ua)
+					last = raiz
+				else:
+					last.insere(last, ini * self.ua, (fim - ini) * self.ua)
+					last = last.prox
+			ini = fim + 1
+
+		self.ll = raiz
+
+
+
+class Fisica:
+	memoria = None
+	alg = 0
+	filaFIFO = []
+	matrizLRU2 = []
+	k = 0
+	vetorLRU4 = []
+	vetorR = []
+
+	def __init__(self, tamanho, ua, pag, alg):
+		self.memoria = Memoria('/tmp/ep3.mem', tamanho, ua, pag)
+		self.alg = alg
+
+		if alg == 3:
+			iniciaLRU2()
+		elif alg == 4:
+			iniciaLRU4()
+
+
+	def substitui(processo):
+		if self.alg == 2:
+			self.FIFO(processo)
+
+	# Mantem uma fila dos processos em ordem de chegada e tira eles nessa ordem
+	# Para inserir um processo na fila so usar append()
+	def FIFO(self, processo):
+		mem = self.memoria.read()
+		if len(self.filaFIFO) > 0:
+			rem = pop(self.filaFIFO).pid # PID do processo mais antigo
+			pos = self.remove(rem) # Posicao do processo removido
+			self.memoria.insere(processo.pid, rem, pos, processo.b)
+		else:
+			print("Mas a fila esta vazia D:")
+
+
+	def LRU2(self, pos):
+		mem = self.memoria.read()
+		somas = []
+		for i in range(len(self.matrizLRU2)):
+			somas.append(sum(self.matrizLRU2[i]))
+
+		# Posicao da menor soma, ou seja, pos do processo que sera removido
+		subst = somas.index(min(somas))
+		self.remove(mem[rem * self.bloco])
+
+
+	def iniciaLRU2(): # LRU 2: Inicia a matriz nxn com 0
+		n = self.memoria.tam/self.memoria.pag
+		for i in range(n):
+			temp = []
+			for j in range(n):
+				temp.append(0)
+			self.matrizLRU2.append(temp)
+
+
+	def atualizaLRU2():
+		for i in range(self.memoria.pag): # Todos da linha = 1
+			self.matrizLRU2[pos][i] = 1
+		for i in range(self.memoria.pag): # Todos da coluna = 0
+			self.matrizLRU2[i][self.k] = 0
+
+		self.k = (self.k + 1) % self.memoria.tam/self.memoria.pag
+
+
+	def LRU4(self):
+		# Se o processo foi acessado na ultima iteracao, soma no seu contador
+		# no bit mais significativo
+		for i in range(len(self.vetorR)):
+			# Esse vetor de R tem que ser pra cada pagina, e nao pra cada processo, aparentemente
+			if self.vetorR[i]:
+				# vetorLRU4 tem o mesmo tamanho do de bit R
+				self.vetorLRU4[i] += 100000000
+			self.vetorLRU4[i] /= 10 # Desloca 1 bit pra direita
+
+		# Remove o menos acessado
+		rem = self.vetorLRU4.index(min(self.vetorLRU4))
+		mem = self.memoria.read()
+		self.memoria.remove(mem[rem * self.bloco])
+		self.memoria.insere
+
+
+	def iniciaLRU4(): # LRU 4: Inicia o vetor de contadores com 0
+		for i in range(self.memoria.tam/self.memoria.pag):
+			vetorLRU4.append(0)
+			vetorR.append(False)
+
+
+	# Recebe uma lista das paginas que foram acessadas nessa iteracao e atualiza
+	# o vetor de bit R
+	def atualizaVetorR(self, acessos):
+		for i in range(len(self.vetorR)):
+			self.vetorR[i] = False
+		for i in acessos:
+			self.vetorR[i] = True
+
+
+
+
+class Virtual:
+	memoria = None
+	alg = 0
+	ql = None
+
+	class QuickList:
+		tams = [] # Tamanhos monitorados
+		espacos = [] # Vetor de vetores. espacos[i] contem as posicoes em q cabe um processo de tamanho tams[i]
+
+		def __init__(self, trace):
+			tam = len(trace)
+
+			for i in range(1, tam):
+				linha = trace[i].split()
+				if len(linha) > 2: # Ignora compactar
+					if not int(linha[2]) in self.tams:
+						self.tams.append(int(linha[2]))
+						self.espacos.append([])
+
+		def atualiza(listaligada):
+			l = es.listaLigada()
+			l = listaligada
+			tamanho = len(self.tams)
+
+			for i in range(tamanho): # Reseta espacos conhecidos
+				self.espacos[i] = []
+			while l != None and l.pos != -1:
+				for i in range(tamanho):
+					if l.tam >= self.tams[i]:
+						self.espacos[i].append(l.pos)
+				l = l.prox
+	
+
+	def __init__(self, tamanho, ua, pag, alg, trace):
+		self.memoria = Memoria('/tmp/ep3.vir', tamanho, ua, pag)
+		self.alg = alg
+
+		if alg == 3: # QuickFit
+			self.ql = QuickList(trace)
+			self.ql.atualiza(self.memoria.ll)
+
+
+	def insere(self, processo):
+		if self.alg == 1:
+			self.bestFit(processo)
+		elif self.alg == 2:
+			self.worstFit(processo)
+		elif self.alg == 3:
+			self.quickFit(processo)
+			self.ql.atualiza(self.memoria.ll)
+
+
+	def bestFit(self, processo):
+		mem = self.memoria.read()
+		best_index = 0
+		best_tam = self.memoria.tam
+		p_tam = processo.b # tamanho do processo
+		ll = self.memoria.ll
+
+		while ll != None and ll.pos != -1:
+			if ll.tam < best_tam and ll.tam > p_tam:
+					best_tam = ll.tam
+					best_index = ll.pos
+			ll = ll.prox
+
+		self.memoria.insere(processo.pid, best_index, p_tam)
+
+
+	def worstFit(self, processo):
+		mem = self.memoria.read()
+		worst_index = 0
+		worst_tam = 0
+		p_tam = processo.b # tamanho do processo
+		ll = self.memoria.ll
+
+		while ll != None and ll.pos != -1:
+			if ll.tam > worst_tam:
+					worst_tam = ll.tam
+					worst_index = ll.pos
+			ll = ll.prox
+
+		if worst_tam < p_tam:
+			print ("Nao ha espaco na memoria")
+
+		self.memoria.insere(processo.pid, worst_index, p_tam)
+
+
+	def quickFit(self, processo):
+		ql = self.ql
+		i = 0
+		for i in range(len(ql.tams)):
+			if processo.b == ql.tams[i]:
+				break
+
+		self.memoria.insere(processo.pid, ql.espacos[i].pop(), processo.b)
--- EP3/simulador.py
+++ EP3/simulador.py
@@ -2,325 +2,8 @@
 # Nome: Andre Ferrari Moukarzel                     NUSP: 9298166	#
 # Nome: Henrique Cerquinho                          NUSP: 9793700	#
 #####################################################################
-import math
+import memoria as mem
 import estruturas as es
-from bitarray import bitarray
-
-ql = None
-
-class QuickList:
-	tams = [] # Tamanhos monitorados
-	espacos = [] # Vetor de vetores. espacos[i] contem as posicoes em q cabe um processo de tamanho tams[i]
-
-	def __init__(self, trace):
-		tam = len(trace)
-
-		for i in range(1, tam):
-			linha = trace[i].split()
-			if len(linha) > 2: # Ignora compactar
-				if not int(linha[2]) in self.tams:
-					self.tams.append(int(linha[2]))
-					self.espacos.append([]) 
-
-
-
-class Memoria:
-	tam = 0
-	arquivo = 0
-	bitmap = 0
-	bloco = 0 # unidade de alocacao na memoria virtual, tamanho da pagina na fisica
-	ll = None # Lista ligada dos espacos livres
-	alg = None # Algoritmo usado na memoria
-
-	filaFIFO = []
-
-	matrizLRU2 = []
-	k = 0
-
-	vetorLRU4 = []
-	vetorR = []
-
-	def __init__(self, nome, tamanho, bloco, alg = None):
-		self.tam = tamanho
-		self.arquivo = nome
-		self.bloco = bloco
-		self.alg = alg
-
-		self.criaArquivo()
-		self.bitmap = ((tamanho/bloco) + 1) * bitarray('0')
-		self.atualizaLL()
-		#self.ll = es.listaLigada()
-		#self.ll.iniRaiz(0, tamanho)
-
-		# Inicializa as estruturas caso seja a memoria fisica
-		if alg == "LRU.2":
-			# LRU 2: Inicia a matriz nxn com 0
-			n = self.tam/self.bloco
-			for i in range(n):
-				temp = []
-				for j in range(n):
-					temp.append(0)
-				self.matrizLRU2.append(temp)
-
-		elif alg == "LRU.4":
-			# LRU 4: Inicia o vetor de contadores com 0
-			for i in range(self.tam/self.bloco):
-				vetorLRU4.append(0)
-				vetorR.append(False)
-
-	def criaArquivo(self):
-		f = open(self.arquivo, 'w+b') # Constroi o arquivo
-		f.close()
-		l = []
-		for i in range (self.tam):
-			l.append(128) # Preenche com -1
-		self.write(l)
-
-
-	def imprime(self):
-		l = self.read()
-		print ('Indice\t|\tPID\t|\tBitmap')
-		for i in range(self.tam):
-			bit = str(int(self.bitmap[int(i/self.bloco)]))
-			if l[i] == 128:
-				if not i%self.bloco:
-					print (str(i) + '\t|\t' + str(-1) + '\t|\t' + bit)
-				else:
-					print (str(i) + '\t|\t' + str(-1) + '\t|')
-			else:
-				if not i%self.bloco:
-					print (str(i) + '\t|\t' + str(l[i]) + '\t|\t' + bit)
-				else:
-					print (str(i) + '\t|\t' + str(l[i]) + '\t|')
-
-
-	# Insere processo de PID que ocupa by bytes, comecando na posicao pos
-	def insere(self, pid, pos, by, preenche = None):
-		l = self.read()
-		b = by
-
-		if preenche: # Para a memoria virtual. Preenche um bloco com o PID do processo
-			b = int(math.ceil(by/float(self.bloco)) * self.bloco)
-
-		for i in range(pos, pos + b): # Atualiza bitmap
-			l[i] = pid
-			self.bitmap[int(i/self.bloco)] = True
-
-		self.atualizaLL()
-		self.write(l)
-
-		# Atualiza a matriz
-		if self.alg == "LRU.2":
-			for i in range(bloco): # Todos da linha = 1
-				self.matrizLRU2[pos][i] = 1
-			for i in range(bloco): # Todos da coluna = 0
-				self.matrizLRU2[i][self.k] = 0
-
-			self.k = (self.k + 1) % self.tam/self.bloco
-
-		# Coloca o processo no fim da fila
-		elif self.alg == "FIFO":
-			self.filaFIFO.append(pid)
-
-
-	# Remove processo com o PID dado
-	def remove(self, pid):
-		l = self.read()
-
-		# Encontra a posicao em que o processo comeca
-		i = 0
-		for i in range(0, self.tam, self.bloco):
-			if l[i] == pid:
-				ret = i
-				break
-		while l[i] == pid and i < self.tam:
-			l[i] = 128
-			self.bitmap[int(i/self.bloco)] = False
-			i += 1
-
-		self.atualizaLL()
-		self.write(l)
-
-
-	# Usado na memoria fisica. Substitui uma pagina com o processo, se necessario
-	def substitui(self, processo):
-		if self.alg == "FIFO":
-			self.FIFO(processo.pid)
-		elif self.alg == "LRU.2":
-			self.LRU2(1) # o argumento eh o quadro de pagina em que o processo se encontra
-		elif self.alg == "LRU.4":
-			self.LRU4(1) # same
-
-
-	# Devolve a memoria como uma lista de PIDs (int)
-	def read(self):
-		lista = []
-
-		with open(self.arquivo, "rb") as f:
-			byte = f.read(1)
-			while byte != "":
-				lista.append(self.bytes_to_int(byte))
-				byte = f.read(1)
-		f.close()
-
-		return lista
-
-
-	# Define a memoria como a lista de PIDs passada como argumento
-	def write(self, lista):
-		binarios = bytearray(lista)
-		f = open(self.arquivo, 'r+')
-		f.write(binarios)
-		f.close()
-
-
-	# Compacta memoria
-	def compactar(self):
-		mem = self.read()
-		compactado = False
-		ocupado = self.tam
-
-		while not compactado:
-			# Procura o primeiro bloco livre
-			cheio = True
-			for i in range(0, self.tam, self.bloco):
-				if mem[i] == 128:
-					cheio = False
-					livre = i
-					break
-			# Ja esta compactado se esta cheio
-			if cheio:
-				break
-
-			# Procura o proximo bloco ocupado
-			for i in range(livre, self.tam, self.bloco):
-				if mem[i] != 128:
-					ocupado = i
-					break
-
-
-			# Move os blocos ocupados para a esquerda
-			while ocupado < self.tam:
-				# Move o bloco inteiro
-				while mem[ocupado] != 128:
-					mem[livre] = mem[ocupado]
-					mem[ocupado] = 128
-
-				livre += 1
-				ocupado += 1
-
-			# Checa se compactou
-			fim = 0
-			for i in range(0, self.tam, self.bloco):
-				compactado = True
-				if mem[i] == 128:
-					fim = 1
-				if fim == 1 and mem[i] != 128:
-					compactado = False
-					break
-
-		for i in range(0, self.tam, self.bloco): # Atualiza o bitmap
-			if mem[i] == 128:
-				self.bitmap[int(i/self.bloco)] = False
-			else:
-				self.bitmap[int(i/self.bloco)] = True
-
-		self.write(mem)
-
-
-	def bytes_to_int(self, bytes):
-		return int(bytes.encode('hex'), 16)
-
-
-	# "Atualiza" a lista ligada que representa o bitmap da memoria criando uma
-	# nova lista do zero e retorna a sua raiz
-	def atualizaLL(self):
-		raiz = es.listaLigada()
-		last = es.listaLigada() # Ultimo elemento da lista
-		ini, fim = 0, 0
-
-		while fim < len(self.bitmap) and ini < len(self.bitmap):
-			# Procura um bloco livre
-			while ini < len(self.bitmap) and self.bitmap[ini]: # bitmap[ini] = 1
-				ini += 1
-			fim = ini + 1
-			# E define seu tamanho
-			while fim < len(self.bitmap) and not self.bitmap[fim]: # bitmap[fim] = 0
-				fim += 1
-
-			if ini < len(self.bitmap) and fim <= len(self.bitmap):
-				if raiz.pos == -1:
-					raiz.iniRaiz(ini * self.bloco, (fim - ini) * self.bloco)
-					last = raiz
-				else:
-					last.insere(last, ini * self.bloco, (fim - ini) * self.bloco)
-					last = last.prox
-			ini = fim + 1
-
-		global ql
-		if self.alg == None and ql != None: # Memoria virtual e quickfit
-			l = es.listaLigada()
-			l = raiz
-			for i in range(len(ql.tams)): # Reseta espacos conhecidos
-				ql.espacos[i] = []
-			while l != None and l.pos != -1:
-				for i in range(len(ql.tams)):
-					if l.tam >= ql.tams[i]:
-						ql.espacos[i].append(l.pos)
-				l = l.prox
-		self.ll = raiz
-
-
-	# SUBSTITUICAO DE PAGINAS #
-
-	# Mantem uma fila dos processos em ordem de chegada e tira eles nessa ordem
-	# Para inserir um processo na fila so usar append()
-	def FIFO(self, pid):
-		mem = self.read()
-		if len(self.filaFIFO) > 0:
-			rem = pop(self.filaFIFO).pid # PID do processo mais antigo
-			pos = self.remove(rem) # Posicao do processo removido
-			self.insere(pid, rem, pos, TAMANHO_DO_PROCESSO, wtf_is_preenche)
-		else:
-			print("Mas a fila esta vazia D:")
-
-
-	def LRU2(self, pos):
-		mem = self.read()
-		somas = []
-		for i in range(len(self.matrizLRU2)):
-			somas.append(sum(self.matrizLRU2[i]))
-
-		# Posicao da menor soma, ou seja, pos do processo que sera removido
-		subst = somas.index(min(somas))
-		self.remove(mem[rem * self.bloco])
-
-
-	def LRU4(self):
-		# Se o processo foi acessado na ultima iteracao, soma no seu contador
-		# no bit mais significativo
-		for i in range(len(self.vetorR)):
-			# Esse vetor de R tem que ser pra cada pagina, e nao pra cada processo, aparentemente
-			if self.vetorR[i]:
-				# vetorLRU4 tem o mesmo tamanho do de bit R
-				self.vetorLRU4[i] += 100000000
-			self.vetorLRU4[i] /= 10 # Desloca 1 bit pra direita
-
-		# Remove o menos acessado
-		rem = self.vetorLRU4.index(min(self.vetorLRU4))
-		mem = self.read()
-		self.remove(mem[rem * self.bloco])
-		self.insere
-
-
-	# Recebe uma lista das paginas que foram acessadas nessa iteracao e atualiza
-	# o vetor de bit R
-	def atualizaVetorR(self, acessos):
-		for i in range(len(self.vetorR)):
-			self.vetorR[i] = False
-		for i in acessos:
-			self.vetorR[i] = True
-
 
 
 def simula(arquivo, espaco, subst, intervalo):
@@ -329,35 +12,22 @@ def simula(arquivo, espaco, subst, intervalo):
 		linhas = f.readlines()
 	linha = linhas[0].split()
 	num = len(linhas) # Numero de linhas no arquivo de trace
-	fis_total = int(linha[0]) # Memoria fisica total
-	vir_total = int(linha[1]) # Memoria virtual total
 	s = int(linha[2]) # Tamanho da unidade de alocacao
 	p = int(linha[3]) # Tamanho da pagina
 	fila = es.FilaDeAcessos()
 	pid_dict = {}
-	esp_alg = espaco
 
-	# Descobre quando que a simulacao acabara
+	# Instancia as memorias
+	vir = mem.Virtual(int(linha[1]), s, p, espaco, linhas)
+	fis = mem.Fisica(int(linha[0]), s, p, subst)
+
+	# Descobre quando que a simulacao vai acabar
 	tempos_finais = []
 	for i in range(1, num):
 		linha = linhas[i].split()
 		if linha[1] != 'COMPACTAR':
 			tempos_finais.append(int(linha[1]))
 	last_tf = max(tempos_finais)
-
-	if espaco == 3: # Quick Fit (PS: esse trecho deve estar antes da inicializacao da memoria)
-		global ql 
-		ql = QuickList(linhas)
-
-	vir = Memoria('/tmp/ep3.vir', vir_total, s)
-	fis = Memoria('/tmp/ep3.mem', fis_total, p, subst)
-
-
-	# TESTEs
-	#bestFit(vir, es.Processo(linhas[1].split()))
-	#bestFit(vir, es.Processo(linhas[2].split()))
-	#es.printLista(vir.ll)
-	#print("RONALDO")
 
 
 	t = 0
@@ -366,13 +36,13 @@ def simula(arquivo, espaco, subst, intervalo):
 	while  True:
 		while i < num and int(linha[0]) == t:
 			if len(linha) == 2 and linha[1] == 'COMPACTAR': # Excecao para COMPACTAR
-				fis.compactar()
-				vir.compactar()
+				fis.memoria.compactar()
+				vir.memoria.compactar()
 			else: # adiciona processo na fila e memoria virtual
 				proc = es.Processo(linha)
 				fila.push(proc)
 				pid_dict[proc.nome] = proc.pid
-				insereVir(proc, esp_alg, vir)
+				vir.insere(proc)
 
 
 			if i < num:
@@ -392,74 +62,18 @@ def simula(arquivo, espaco, subst, intervalo):
 			if len(temp) > 2 and int(temp[1]) == t: # tf = t
 				pid = pid_dict.get(temp[3])
 				#fis.remove(pid)
-				vir.remove(pid)
+				vir.memoria.remove(pid)
 
 
 		if t % intervalo == 0: # Imprime informacoess
 			print (30 * '-' + '\nTempo = ' + str(t) + '\n')
 			print ('Memoria Virtual')
-			vir.imprime()
+			vir.memoria.imprime()
 			print ('\nMemoria Fisica')
-			fis.imprime()
+			fis.memoria.imprime()
 			print ('\n' + 30 * '-' + '\n')
 
 		if i == num and t >= last_tf: # Todos os processos acabaram sua execucao
 			break
 
 		t += 1
-
-
-# GERENCIAMENTO DE MEMORIA #
-def insereVir(processo, alg, memoria):
-	if alg == 1:
-		bestFit(memoria, processo)
-	elif alg == 2:
-		worstFit(memoria, processo)
-	else:
-		quickFit(memoria, processo)
-
-
-
-def bestFit(memoria, processo):
-	mem = memoria.read()
-	best_index = 0
-	best_tam = memoria.tam
-	p_tam = processo.b # tamanho do processo
-	ll = memoria.ll
-
-	while ll != None and ll.pos != -1:
-		if ll.tam < best_tam and ll.tam > p_tam:
-				best_tam = ll.tam
-				best_index = ll.pos
-		ll = ll.prox
-
-	memoria.insere(processo.pid, best_index, p_tam, True)
-
-
-def worstFit(memoria, processo):
-	mem = memoria.read()
-	worst_index = 0
-	worst_tam = 0
-	p_tam = processo.b # tamanho do processo
-	ll = memoria.ll
-
-	while ll != None:
-		if ll.tam > worst_tam:
-				worst_tam = ll.tam
-				worst_index = ll.pos
-		ll = ll.prox
-
-	if worst_tam < p_tam:
-		print ("Nao ha espaco na memoria")
-
-	memoria.insere(processo.pid, worst_index, p_tam, True)
-
-
-def quickFit(memoria, processo):
-	global ql
-	i = 0
-	for i in range(len(ql.tams)):
-		if processo.b == ql.tams[i]:
-			break
-
-	memoria.insere(processo.pid, ql.espacos[i].pop(), processo.b, True)
